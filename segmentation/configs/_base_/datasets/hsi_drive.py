32cb4c2f30c6df5e863c26a847180533eabb698d
root <j.gutierrez@ehu.eus>
2023-09-22 09:15:32 +0200

    (fu) configs/_base_/datasets/hsi_drive.py

--- segmentation/configs/_base_/datasets/hsi_drive.py
+++ segmentation/configs/_base_/datasets/hsi_drive.py
@@ -1,35 +1,21 @@
-#img_norm_cfg = dict(
-#    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-#crop_size = (512, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    #dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
-    #dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    #dict(type='RandomFlip', flip_ratio=0.5),
-    #dict(type='PhotoMetricDistortion'),
-    #dict(type='Normalize', **img_norm_cfg),
-    #dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
-    #dict(type='DefaultFormatBundle'),
+    dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_semantic_seg'], meta_keys=('filename', 'ori_filename', 'ori_shape', 'img_shape', 'pad_shape', 'scale_factor', 'img_norm_cfg'))
-    #dict(type='PackSegInputs')
 ]
 test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations'),
-    #dict(
-        #type='MultiScaleFlipAug',
-        #img_scale=(2048, 1024),
+        dict(type='LoadImageFromFile'),
+        dict(
+        type='MultiScaleFlipAug',
+        img_scale=(208, 400),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
-        #flip=False,
-        #transforms=[
-        #    dict(type='Resize', keep_ratio=True),
-        #    dict(type='RandomFlip'),
-        #    dict(type='Normalize', **img_norm_cfg),
-        #    dict(type='ImageToTensor', keys=['img']),
-        #    dict(type='Collect', keys=['img']),
-        #])
-    #dict(type='PackSegInputs')
+        flip=False,
+        transforms=[
+            #dict(type='Resize', keep_ratio=True),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
 ]
 
 data = dict(
@@ -55,4 +41,4 @@ data = dict(
         pipeline=test_pipeline))
 
 val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'], ignore_index=0)
-test_evaluator = val_evaluator
+#test_evaluator = val_evaluator
