b78b662ad3c91207e2442344c45011d0c7d1690a
Jon <j.gutierrez@ehu.eus>
2023-09-25 13:15:23 +0200

    updated .../configs/hsidrive/upernet_internimage_t_512x1024_160k_hsidrive.py to remove some unnecessary preprocessing, add class_weight and ignore unlabelled pixels

--- segmentation/configs/hsidrive/upernet_internimage_t_512x1024_160k_hsidrive.py
+++ segmentation/configs/hsidrive/upernet_internimage_t_512x1024_160k_hsidrive.py
@@ -5,10 +5,8 @@
 # --------------------------------------------------------
 _base_ = [
     '../_base_/models/upernet_r50.py', '../_base_/datasets/hsi_drive.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_jon.py'
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
-
-data_preprocessor = dict(type='SegDataPreProcessor', mean=None, std=None, bgr_to_rgb=None, pad_val=0, seg_pad_val=255)
 
 #pretrained = 'https://huggingface.co/OpenGVLab/InternImage/resolve/main/internimage_t_1k_224.pth'
 model = dict(
@@ -30,8 +28,34 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         #init_cfg=dict(type='Pretrained', checkpoint=pretrained)
         ),
-    decode_head=dict(num_classes=6, in_channels=[64, 128, 256, 512]),
-    auxiliary_head=dict(num_classes=6, in_channels=256),
+    decode_head=dict(num_classes=6, in_channels=[64, 128, 256, 512],
+                loss_decode=dict(
+                    avg_non_ignore=True,
+                    class_weight=[
+                        0,
+                        0.023432840583372,
+                        0.487242396681053,
+                        0.059020865556711,
+                        0.296974520051312,
+                        0.1333293771275520
+                    ],
+                loss_weight=1.0,
+                type='CrossEntropyLoss',
+                use_sigmoid=False)),
+    auxiliary_head=dict(num_classes=6, in_channels=256,
+                loss_decode=dict(
+                    avg_non_ignore=True,
+                    class_weight=[
+                        0,
+                        0.023432840583372,
+                        0.487242396681053,
+                        0.059020865556711,
+                        0.296974520051312,
+                        0.1333293771275520
+                    ],
+                    loss_weight=1.0,
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False)),
     test_cfg=dict(mode='whole')
 )
 optimizer = dict(
@@ -44,6 +68,21 @@ lr_config = dict(_delete_=True, policy='poly',
                  warmup_iters=1500,
                  warmup_ratio=1e-6,
                  power=1.0, min_lr=0.0, by_epoch=False)
+
+
+runner = dict(type='IterBasedRunner')
+checkpoint_config = dict(by_epoch=False, interval=1000, max_keep_ckpts=1)
+evaluation = dict(interval=16000, metric='mIoU', save_best='mIoU')
+
 # By default, models are trained on 8 GPUs with 2 images per GPU
-data=dict(samples_per_gpu=8)
+data=dict(samples_per_gpu=32,
+          workers_per_gpu=4,
+        train=dict(
+        data_root='/data/'),
+        val=dict(
+        data_root='/data/'),
+        test=dict(
+        data_root='/data/'))
+
 # fp16 = dict(loss_scale=dict(init_scale=512))
+
